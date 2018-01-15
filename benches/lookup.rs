ebf6962bc93f00b4ef27433e3b60a662f783b165
NotBad4U <alessio.coltellacci@clever-cloud.com>
2018-01-16 00:44:48 +0100

    Add criterion dep

--- benches/lookup.rs
+++ benches/lookup.rs
@@ -5,12 +5,15 @@ extern crate flame;
 extern crate uuid;
 extern crate rand;
 extern crate test;
+#[macro_use]
+extern crate criterion;
 
 use trie::*;
 use uuid::Uuid;
 use rand::XorShiftRng;
 use rand::Rng;
 use test::Bencher;
+use criterion::Criterion;
 
 static NB_ELEM_SEED: i32 = 10000;
 
@@ -194,44 +197,47 @@ fn seed_known_domain(root: &mut TrieNode<u8>) {
     root.domain_insert(Vec::from(&b"axolema.washe-pote.rs"[..]), 5);
 }
 
+fn bench_look(c: &mut Criterion) {
+    c.bench_function("lookup on registred domains", |b| {
+        let mut root: TrieNode<u8> = TrieNode::root();
+        seed_bench_trie(&mut root);
+        seed_known_domain(&mut root);
 
-#[bench]
-fn bench_look(b: &mut Bencher) {
-    let mut root: TrieNode<u8> = TrieNode::root();
-    seed_bench_trie(&mut root);
-    seed_known_domain(&mut root);
-
-    b.iter(|| {
-        root.domain_lookup(b"Washtucna.obeliskoide.org");
-        root.domain_lookup(b"co-adjust.walll-fed.net");
-        root.domain_lookup(b"axonne.coadminnistration.gov");
-        root.domain_lookup(b"axofugal.obelis.com");
-        root.domain_lookup(b"washwomean.coadjuvant.mil");
-        root.domain_lookup(b"obeliske.coadjuv.io");
-        root.domain_lookup(b"coadunatione.coadministration.th");
-        root.domain_lookup(b"axolemma.aaaaxole.ca");
-        root.domain_lookup(b"washtail.coadeejute.au");
-        root.domain_lookup(b"axolema.washe-pote.rs");
-    })
+        b.iter(|| {
+            root.domain_lookup(b"Washtucna.obeliskoide.org");
+            root.domain_lookup(b"co-adjust.walll-fed.net");
+            root.domain_lookup(b"axonne.coadminnistration.gov");
+            root.domain_lookup(b"axofugal.obelis.com");
+            root.domain_lookup(b"washwomean.coadjuvant.mil");
+            root.domain_lookup(b"obeliske.coadjuv.io");
+            root.domain_lookup(b"coadunatione.coadministration.th");
+            root.domain_lookup(b"axolemma.aaaaxole.ca");
+            root.domain_lookup(b"washtail.coadeejute.au");
+            root.domain_lookup(b"axolema.washe-pote.rs")
+        })
+    });
 }
 
+fn bench_lookup_on_unknown(c: &mut Criterion) {
+    c.bench_function("lookup on unregistred domains", |b| {
+        let mut root: TrieNode<u8> = TrieNode::root();
+        seed_bench_trie(&mut root);
+        seed_known_domain(&mut root);
 
-#[bench]
-fn bench_lookup_on_unknown(b: &mut Bencher) {
-    let mut root: TrieNode<u8> = TrieNode::root();
-    seed_bench_trie(&mut root);
-    seed_known_domain(&mut root);
-
-    b.iter(|| {
-        root.domain_lookup(b"sozu.org");
-        root.domain_lookup(b"yolo.toto.net");
-        root.domain_lookup(b"foo.kill.gov");
-        root.domain_lookup(b"riche.riche.com");
-        root.domain_lookup(b"unknown.zelda.mil");
-        root.domain_lookup(b"tracktl.yolo.io");
-        root.domain_lookup(b"rebase.arnaud.th");
-        root.domain_lookup(b"never.gonna.ca");
-        root.domain_lookup(b"clever.cloud.au");
-        root.domain_lookup(b"book.mac.rs");
-    })
+        b.iter(|| {
+            root.domain_lookup(b"sozu.org");
+            root.domain_lookup(b"yolo.toto.net");
+            root.domain_lookup(b"foo.kill.gov");
+            root.domain_lookup(b"riche.riche.com");
+            root.domain_lookup(b"unknown.zelda.mil");
+            root.domain_lookup(b"tracktl.yolo.io");
+            root.domain_lookup(b"rebase.arnaud.th");
+            root.domain_lookup(b"never.gonna.ca");
+            root.domain_lookup(b"clever.cloud.au");
+            root.domain_lookup(b"book.mac.rs")
+        })
+    });
 }
+
+criterion_group!(lookup, bench_look, bench_lookup_on_unknown);
+criterion_main!(lookup);
