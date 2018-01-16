9e5ed1302223ca51f482ac4b6def8c3ff638378c
NotBad4U <alessio.coltellacci@clever-cloud.com>
2018-01-16 16:52:24 +0100

    Add lookup bench

---
+++ examples/lookup.rs
@@ -0,0 +1,40 @@
+#[macro_use]
+extern crate trie;
+
+use trie::*;
+use trie::gen_seed::*;
+
+static NB_LOOKUP: i32 = 1000;
+static NB_ELEM_SEED: i32 = 10000;
+
+fn seed_known_domain(root: &mut TrieNode<u8>) {
+    root.domain_insert(Vec::from(&b"axofugal.obelis.com"[..]), 5);
+    root.domain_insert(Vec::from(&b"Washtucna.obeliskoide.org"[..]), 5);
+    root.domain_insert(Vec::from(&b"co-adjust.walll-fed.net"[..]), 5);
+    root.domain_insert(Vec::from(&b"axonne.coadminnistration.gov"[..]), 5);
+    root.domain_insert(Vec::from(&b"washwomean.coadjuvant.mil"[..]), 5);
+    root.domain_insert(Vec::from(&b"obeliske.coadjuv.io"[..]), 5);
+    root.domain_insert(Vec::from(&b"coadunatione.coadministration.th"[..]), 5);
+    root.domain_insert(Vec::from(&b"axolemma.aaaaxole.ca"[..]), 5);
+    root.domain_insert(Vec::from(&b"washtail.coadeejute.au"[..]), 5);
+    root.domain_insert(Vec::from(&b"axolema.washe-pote.rs"[..]), 5);
+}
+
+fn main() {
+ let mut root: TrieNode<u8> = TrieNode::root();
+        seed_bench_trie(&mut root, NB_ELEM_SEED);
+        seed_known_domain(&mut root);
+
+    for _ in 0..NB_LOOKUP {
+        root.domain_lookup(b"Washtucna.obeliskoide.org");
+        root.domain_lookup(b"co-adjust.walll-fed.net");
+        root.domain_lookup(b"axonne.coadminnistration.gov");
+        root.domain_lookup(b"axofugal.obelis.com");
+        root.domain_lookup(b"washwomean.coadjuvant.mil");
+        root.domain_lookup(b"obeliske.coadjuv.io");
+        root.domain_lookup(b"coadunatione.coadministration.th");
+        root.domain_lookup(b"axolemma.aaaaxole.ca");
+        root.domain_lookup(b"washtail.coadeejute.au");
+        root.domain_lookup(b"axolema.washe-pote.rs");
+    }
+}
