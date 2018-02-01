2ac52cfe21c1c2df5d0afbddc9ee1b06cc7fe859
Geoffroy Couprie <geo.couprie@gmail.com>
2018-02-01 19:23:12 +0100

    Experiment 2: separates the keys and the children nodes in 2 vectors

---
+++ examples/experiment2.rs
@@ -0,0 +1,37 @@
+extern crate trie;
+
+use trie::experiment2_trie::*;
+
+static NB_ELEM_SEED: i32 = 10000;
+
+fn seed_known_domain(root: &mut TrieNode<u8>) {
+    root.domain_insert(Vec::from(&b"axofugal.obelis.com"[..]), 5);
+    root.domain_insert(Vec::from(&b"Washtucna.obeliskoide.org"[..]), 5);
+    root.domain_insert(Vec::from(&b"co-adjust.walll-fed.net"[..]), 5);
+    root.domain_insert(Vec::from(&b"axonne.coadminnistration.gov"[..]), 5);
+    root.domain_insert(Vec::from(&b"washwomean.coadjuvant.mil"[..]), 5);
+    root.domain_insert(Vec::from(&b"obeliske.coadjuv.io"[..]), 5);
+    root.domain_insert(Vec::from(&b"coadunatione.coadministration.th"[..]), 5);
+    root.domain_insert(Vec::from(&b"axolemma.aaaaxole.ca"[..]), 5);
+    root.domain_insert(Vec::from(&b"washtail.coadeejute.au"[..]), 5);
+    root.domain_insert(Vec::from(&b"axolema.washe-pote.rs"[..]), 5);
+}
+
+fn main() {
+ let mut root: TrieNode<u8> = TrieNode::root();
+        seed_bench_trie(&mut root, NB_ELEM_SEED);
+        seed_known_domain(&mut root);
+
+    loop {
+        root.domain_lookup(b"Washtucna.obeliskoide.org");
+        root.domain_lookup(b"co-adjust.walll-fed.net");
+        root.domain_lookup(b"axonne.coadminnistration.gov");
+        root.domain_lookup(b"axofugal.obelis.com");
+        root.domain_lookup(b"washwomean.coadjuvant.mil");
+        root.domain_lookup(b"obeliske.coadjuv.io");
+        root.domain_lookup(b"coadunatione.coadministration.th");
+        root.domain_lookup(b"axolemma.aaaaxole.ca");
+        root.domain_lookup(b"washtail.coadeejute.au");
+        root.domain_lookup(b"axolema.washe-pote.rs");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,5 @@ pub mod seed;
 pub mod gen_seed;
 pub mod sozu_trie;
 pub mod experiment1_trie;
+pub mod experiment2_trie;
 
