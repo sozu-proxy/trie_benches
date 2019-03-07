720d55bdf642b2761a30e241cf8f5fc1f6af79fa
Geoffroy Couprie <geo.couprie@gmail.com>
2019-03-07 16:31:43 +0100

    add more domains in the "filling" test

--- benches/filling.rs
+++ benches/filling.rs
@@ -9,7 +9,7 @@ static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 //use trie::experiment1_trie::*;
 use trie::DomainLookup;
-use trie::gen_seed::seed_bench_trie;
+use trie::gen_seed::*;
 use criterion::{Criterion, ParameterizedBenchmark};
 
 
@@ -21,31 +21,38 @@ fn bench_fill(c: &mut Criterion) {
       ParameterizedBenchmark::new("exp 1", |b, n| b.iter(|| {
           let mut root: trie::experiment1_trie::TrieNode<u8> = trie::experiment1_trie::TrieNode::root();
           seed_bench_trie(&mut root, *n);
+          seed_known_domain(&mut root);
         }), vec![nb_elems_seed])
       .with_function("exp2", |b, n| b.iter(|| {
         let mut root: trie::experiment2_trie::TrieNode<u8> = trie::experiment2_trie::TrieNode::root();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
       }))
       .with_function("exp3", |b, n| b.iter(|| {
         let mut root: trie::experiment3_trie::TrieNode<u8> = trie::experiment3_trie::TrieNode::root();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
       }))
       .with_function("exp4", |b, n| b.iter(|| {
         let mut root: trie::experiment4_fst::Machine<u8> = trie::experiment4_fst::Machine::new();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
         root.finish();
       }))
       .with_function("sozu", |b, n| b.iter(|| {
         let mut root: trie::sozu_trie::TrieNode<u8> = trie::sozu_trie::TrieNode::root();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
       }))
       .with_function("linear", |b, n| b.iter(|| {
         let mut root: trie::linear::List<u8> = trie::linear::List::root();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
       }))
       .with_function("hashmap", |b, n| b.iter(|| {
         let mut root: trie::hashmap::Map = trie::hashmap::Map::new();
         seed_bench_trie(&mut root, *n);
+        seed_known_domain(&mut root);
       }))
     );
 }
